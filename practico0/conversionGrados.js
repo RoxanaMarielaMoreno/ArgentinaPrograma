e5e8e1ae03b04eca2245a2626cbf9ebec905e261
Marcos <garencabeza@gmail.com>
2023-06-01 22:37:37 -0300

    AgregoElArchivoConversiongrados

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -1,12 +1,12 @@
 let fahrenheit = 75;
-let celsius = (fahrenheit - 32) * 5/9 ;
+let celsius = (fahrenheit - 32) * 5/9 
 
 
 console.log(celsius);
 
 
 let fahrenheit2 = 50;
-let celsius2 = (fahrenheit - 32) * 5/9 ;
+let celsius2 = (fahrenheit2 - 32) * 5/9 
 
 
 console.log(celsius2);
