97f2acdd4f748df4a3696a3ebab40f05e03ecd2c
Marcos <garencabeza@gmail.com>
2023-06-01 22:26:39 -0300

    coreccionn

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -2,7 +2,7 @@ let base = 5 ;
 let altura = 3;
 
 
-let area = base*altura;
+let area = base*altura
 
 console.log(area);
 
@@ -10,6 +10,6 @@ let base2 = 12.5 ;
 let altura2 = 5.3 ;
 
 
-let area2 = base2*altura2;
+let area2 = base2*altura2
 
 console.log(area2);
