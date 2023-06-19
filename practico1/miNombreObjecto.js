626d842d19574e49d2265efa89ccec3b4c280c88
Marcos <garencabeza@gmail.com>
2023-06-18 23:45:57 -0300

    practico1completo

---
+++ practico1/miNombreObjecto.js
@@ -0,0 +1,6 @@
+var persona = new Object();
+persona.nombre = "Roxana";
+persona.apellido = "Moreno";
+persona.años = 47;
+
+console.log("Mi objeto persona es el siguiente: " + JSON.stringify(persona));
---
+++ practico1/ninombrecompleto.js
@@ -0,0 +1,7 @@
+let nombre = "Roxana";
+let apellido = "Moreno";
+let edad = 47;
+
+
+
+console.log("Hola mi nombre es " + nombre + " " + apellido + "."+" "+"Tengo "+ edad + " " + "años");
